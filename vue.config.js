02127d528f409cdff990bdf511daf09549e01747
Kishan Kumar <kumar.kishan@outlook.com>
2022-02-11 10:51:37 +0530

    Public path for github pages

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   outputDir: "docs",
+  publicPath: "/inr-converter/",
   pluginOptions: {
     moment: {
       locales: ["en"],
     },
   },
+
   chainWebpack: (config) => {
     config.plugin("html").tap((args) => {
       args[0].title = "INR Currency Converter";
